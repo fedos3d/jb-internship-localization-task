0defa86c7d87b819041227dcac0fe173d46f2d7d
Fedor Anikeev <37173454+fedos3d@users.noreply.github.com>
2021-04-22 02:34:33 +0300

    Update FailedPaymentEmail.kt

--- src/main/kotlin/FailedPaymentEmail.kt
+++ src/main/kotlin/FailedPaymentEmail.kt
@@ -10,38 +10,39 @@ class FailedPaymentEmail(
 ) {
     fun buildContent(body: HTML, lang: Locale) = with(body) {
         val mem = UResourceBundle.getBundleInstance("Languages", ULocale.forLocale(lang));
-        val msgFmt = MessageFormat(mem.getString("podp"),ULocale.forLocale(lang))
         val msgFmt2 = MessageFormat(mem.getString("subs"), ULocale.forLocale(lang))
         val msgFmt3 = MessageFormat(mem.getString("podpplur"), ULocale.forLocale(lang))
+        val msgFmt4 = MessageFormat(mem.getString("s2"), ULocale.forLocale(lang))
+        val hashMap4 = hashMapOf<String, Any>();
+        hashMap4.put("num", data.items.size)
+        hashMap4.put("period", mem.getString(data.subscriptionPack.billingPeriod.toString()))
+        hashMap4.put("card", (data.cardDetails ?: mem.getString("s3")))
+        hashMap4.put("items", data.items.joinToString(
+            ", "
+        ) { it.productName })
         val hashmap3 = hashMapOf<String, Any>();
+        hashmap3.put("card", (data.cardDetails ?: mem.getString("s3")))
         hashmap3.put("num", data.items.sumBy { it.quantity })
-        val hashMap = hashMapOf<String, Any>()
-        hashMap.put("num", data.items.size)
-        hashMap.put("period", mem.getString(data.subscriptionPack.billingPeriod.toString()))
+        hashmap3.put("ref", data.subscriptionPack.subPackRef?.let { "#$it" }.orEmpty())
+        hashmap3.put("period", (when (data.subscriptionPack.billingPeriod) {
+            BillingPeriod.MONTHLY -> mem.getString("month")
+            BillingPeriod.ANNUAL -> mem.getString("year")
+            else -> mem.getString("otherp")
+        })
+        )
         val hasMap2 = hashMapOf<String, Any>();
+        hasMap2.put("date", DateTimeFormatter.ofPattern("MMM dd, YYYY", lang).format(data.paymentDeadline))
         hasMap2.put("num", data.subscriptionPack.totalLicenses)
+
             body {
                 p {
                     +mem.getString("s1");
                 }
                 p {
-                    + mem.getString("s2")
-                    + (data.cardDetails ?: mem.getString("s3"))
                     if (data.customerType == CustomerType.PERSONAL) {
-                        +"${msgFmt.format(hashMap)} "//mem.getString("s4"))
-                        + data.items.joinToString(
-                            ", "
-                        ) { it.productName }
+                        + msgFmt4.format(hashMap4)
                     } else {
                         +msgFmt3.format(hashmap3)
-                        +"${mem.getString("s6")} ${
-                            data.subscriptionPack.subPackRef?.let { "#$it" }.orEmpty()
-                        } ${mem.getString("s7")} "
-                        +(when (data.subscriptionPack.billingPeriod) {
-                            BillingPeriod.MONTHLY -> mem.getString("month")
-                            BillingPeriod.ANNUAL -> mem.getString("year")
-                            else -> mem.getString("otherp")
-                        } + ": ")
                         br()
                         data.items.forEach {
                             +"- ${it.quantity} x ${mem.getString(it.description.replace(" ", "_"))}";br()
@@ -55,11 +56,14 @@ class FailedPaymentEmail(
                     creditCardFailedPaymentReasons(mem)
 
                 p {
-                    +(msgFmt2.format(hasMap2))
+                    val kek = msgFmt2.format(hasMap2)
+                    val fpart = kek.substringBefore(mem.getString("manually"))
+                    val spart = kek.substringAfter(mem.getString("manually"))
+                    +fpart
                     a(href = "https://foo.bar/ex") {
-                        +mem.getString("manually")
-                    }
-                    +"${mem.getString("till")} ${DateTimeFormatter.ofPattern("MMM dd, YYYY", lang).format(data.paymentDeadline)}"
+                          +mem.getString("manually")
+                        }
+                    +spart
                 }
                 p {
                    +mem.getString("doublecheck")
